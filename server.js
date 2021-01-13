9487283a8953aeece587b4933237bc45700c413e
Luroz <luisrpirela@gmail.com>
2021-01-13 00:26:37 -0400

    Server y get de usuarios

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors");
+const dotenv = require("dotenv");
 
+let db = require("./src/db/database.js")
 
+dotenv.config();
 const app = express()
 
+//middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(function(req, res){
+    res.status(404);
+});
 // Port
 var HTTP_PORT = 8000 
 
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
-// Root 
+
+//** ROUTES */
+//Test
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
+//get users
+    app.get("/api/users", (req, res, next) => {
+        var sql = "select * from user"
+        var params = []
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+            }
+            res.json({
+                "message":"success",
+                "data":rows
+            })
+        });
+    });
 
 
-app.use(function(req, res){
-    res.status(404);
-});
 
--- src/db/database.js
+++ src/db/database.js
@@ -1,32 +1,34 @@
-var sqlite3 = require('sqlite3').verbose()
-var md5 = require('md5')
+const sqlite3 = require('sqlite3').verbose()
+const md5 = require('md5')
 
 const DB_SOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DB_SOURCE, (err) => {
     if (err) {
       // Cannot open database
-      console.error(err.message)
-      throw err
+    console.error(err.message)
+    throw err
     }else{
-        console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
+        db.run(`CREATE TABLE IF NOT EXIST user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text, 
             email text UNIQUE, 
             password text, 
+            age INTEGER
             CONSTRAINT email_unique UNIQUE (email)
             )`,
         (err) => {
             if (err) {
                 // Table already created
             }else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
-                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
-                db.run(insert, ["user","user@example.com",md5("user123456")])
+                
+                var insert = 'INSERT INTO user (name, email, password, age) VALUES (?,?,?,?)'
+
+                db.run(insert, ["admin","admin@example.com",md5("admin123456"), 25])
+                console.log("first insert")
             }
         });  
+        console.log('Connected to the SQLite database.')
     }
 });
 
